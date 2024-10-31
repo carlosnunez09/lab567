70da5b673913b185390ab28aff3caa1d5ba7ec90
Terrence <21steeleT@gmail.com>
2024-10-31 14:17:31 -0400

    updating GUI

--- src/main/java/gui/GUI2.java
+++ src/main/java/gui/GUI2.java
@@ -1,14 +1,33 @@
 package gui;
 
+import environment.Environment;
+import lifeform.Alien;
+import lifeform.Human;
+import lifeform.LifeForm;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
 
+
+
 public class GUI2 {
+
   public static void main(String[] args) {
-    var tv = new TV();
-    var r = new SimpleRemote(() -> tv.toggle());;
+    Environment e = new Environment(10, 10);
+    Alien jeff = new Alien("Jeff", 15);
+    Human bill = new Human("Bill", 15, 5);
+
+    e.addLifeForm(jeff, 5, 5);
+    e.addLifeForm(bill, 2, 2);
+
+    jeff.setLocation(5, 5);
+    bill.setLocation(2,2);
+
+
+    var tv = new TV(e);
+    //var r = new SimpleRemote(() -> tv.toggle());;
   }
 }
 
@@ -20,19 +39,19 @@ class TV extends JFrame {
   JLabel label;
   boolean off = true;
   boolean up = true;
-  int col = 15;
-  int row = 15;
+  int col = 10;
+  int row = 10;
   JButton[][] numberOfButtons = new JButton[row][col];
 
   ImageIcon offImage = new ImageIcon(new BufferedImage(355, 200,
-          BufferedImage.TYPE_3BYTE_BGR));
+         BufferedImage.TYPE_3BYTE_BGR));
   ImageIcon onImage = new ImageIcon("news.gif");
   ImageIcon upImage = new ImageIcon("gif.gif");
 
   JTextArea remoteDisplay;
   JTextArea textArea;
 
-  public TV() {
+  public TV(Environment env) {
     setSize(800, 1000);
     setLayout(new BorderLayout());
 
@@ -49,11 +68,14 @@ class TV extends JFrame {
     textArea.setBackground(Color.GRAY);
     textArea.setEditable(false);
 
-    for (int i = 0; i < row; i++){
-      for (int j = 0; j < col; j++){
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
         numberOfButtons[i][j] = new JButton();
         //numberOfButtons[i][j].setFocusable(false);
         numberOfButtons[i][j].addActionListener(this::actionPerformed);
+        if (env.getLifeForm(i, j) != null) {
+          numberOfButtons[i][j].setBackground(Color.BLACK);
+        }
         panel.add(numberOfButtons[i][j]);
       }
     }
@@ -66,7 +88,6 @@ class TV extends JFrame {
     //add(sideBar, BorderLayout.WEST);
 
 
-
     //label = new JLabel(offImage);
 
     //panel.add(label);
@@ -77,11 +98,12 @@ class TV extends JFrame {
   }
 
 
-  public void actionPerformed(ActionEvent e){
+  public void actionPerformed(ActionEvent e) {
     for (int i = 0; i < row; i++) {
       for (int j = 0; j < col; j++) {
         if (e.getSource() == numberOfButtons[i][j]) {
           numberOfButtons[i][j].setBackground(Color.YELLOW);
+
           //textArea.setText("A button was pressed\n");
         }
       }
@@ -100,7 +122,6 @@ class TV extends JFrame {
 
      // Draw a line from (50, 50) to (200, 200)
   }*/
-
   public void toggle() {
     if (!off) {
       off();
@@ -127,57 +148,57 @@ class TV extends JFrame {
   }
 }
 
-class SimpleRemote extends JFrame {
-  Command c;
-  JTextArea text;
+  class SimpleRemote extends JFrame {
+    Command c;
+    JTextArea text;
 
-  public SimpleRemote(Command cc) {
-    c = cc;
-    setLayout(new BorderLayout());
+    public SimpleRemote(Command cc) {
+      c = cc;
+      setLayout(new BorderLayout());
 
-    JButton b = new JButton("Move");
-    //b.setSize(5, 5);
-    b.setBounds(250, 100, 50, 5);
-    add(b, BorderLayout.CENTER);
+      JButton b = new JButton("Move");
+      //b.setSize(5, 5);
+      b.setBounds(250, 100, 50, 5);
+      add(b, BorderLayout.CENTER);
 
-    JButton attack = new JButton("Attack");
-    //attack.setSize(5, 5);
-    attack.setBounds(350, 100, 50, 5);
-    add(attack, BorderLayout.CENTER);
+      JButton attack = new JButton("Attack");
+      //attack.setSize(5, 5);
+      attack.setBounds(350, 100, 50, 5);
+      add(attack, BorderLayout.CENTER);
 
-    JButton up = new JButton("East");
-    //up.setSize(5, 5);
-    up.setBounds(400, 100, 100, 5);
-    add(up, BorderLayout.EAST);
+      JButton up = new JButton("East");
+      //up.setSize(5, 5);
+      up.setBounds(400, 100, 100, 5);
+      add(up, BorderLayout.EAST);
 
-    JButton down = new JButton("West");
-    //down.setSize(5, 5);
-    down.setBounds(100, 200, 100, 5);
-    add(down, BorderLayout.WEST);
+      JButton down = new JButton("West");
+      //down.setSize(5, 5);
+      down.setBounds(100, 200, 100, 5);
+      add(down, BorderLayout.WEST);
 
-    JButton North = new JButton("North");
-    //North.setSize(5, 5);
-    North.setBounds(100, 300, 100, 5);
-    add(North, BorderLayout.NORTH);
+      JButton North = new JButton("North");
+      //North.setSize(5, 5);
+      North.setBounds(100, 300, 100, 5);
+      add(North, BorderLayout.NORTH);
 
-    JButton South = new JButton("South");
-    //South.setSize(10, 10);
-    South.setBounds(100, 400, 100, 5);
-    add(South, BorderLayout.SOUTH);
+      JButton South = new JButton("South");
+      //South.setSize(10, 10);
+      South.setBounds(100, 400, 100, 5);
+      add(South, BorderLayout.SOUTH);
 
-    b.addActionListener(a -> c.execute());
-    up.addActionListener(a -> c.execute());
-    down.addActionListener(a -> c.execute());
-    North.addActionListener(a -> c.execute());
-    South.addActionListener(a -> c.execute());
-    attack.addActionListener(a -> c.execute());
+      b.addActionListener(a -> c.execute());
+      up.addActionListener(a -> c.execute());
+      down.addActionListener(a -> c.execute());
+      North.addActionListener(a -> c.execute());
+      South.addActionListener(a -> c.execute());
+      attack.addActionListener(a -> c.execute());
 
-    setSize(500, 500);
-    setVisible(true);
-    setLocation(400, 400);
+      setSize(500, 500);
+      setVisible(true);
+      setLocation(400, 400);
+    }
+
+    public JTextArea getTextArea() {
+      return text;
+    }
   }
-
-  public JTextArea getTextArea() {
-    return text;
-  }
-}
