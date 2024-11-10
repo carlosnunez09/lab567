351fab6b38f2328472f4c62805009ba08297a584
johnripper <jr2080@ship.edu>
2024-11-10 18:08:59 -0500

    did acquire1cmd

---
+++ src/main/java/command/Acquire1Cmd.java
@@ -0,0 +1,56 @@
+package command;
+
+import environment.Cell;
+import environment.Environment;
+import exceptions.EnvironmentException;
+import exceptions.WeaponException;
+import lifeform.LifeForm;
+import weapon.Weapon;
+
+public class Acquire1Cmd implements Command {
+
+  private Environment environment;
+
+  public Acquire1Cmd(Environment environment) { this.environment = environment;}
+
+
+
+
+
+
+  @Override
+  public void execute(int row, int col) throws WeaponException, EnvironmentException {
+//        if (environment.getLifeForm(row, col) != null) {
+//
+//      if (environment.getWeapons()) {
+//          environment.addWeapon();
+//      }
+
+
+    Cell cell = environment.getCell(row, col);
+
+    LifeForm lifeForm = cell.getLifeForm();
+
+    if (lifeForm != null) {
+      Weapon weapon1 = cell.getWeapon1();
+      if (weapon1 != null) {
+        if (lifeForm.hasWeapon()) {
+          Weapon currentWeapon = lifeForm.getWeapon();
+          lifeForm.pickUpWeapon(weapon1);
+          cell.addWeapon(currentWeapon);
+        } else {
+          lifeForm.pickUpWeapon(weapon1);
+        }
+        cell.removeWeapon(weapon1);
+      }
+    }
+
+
+    }
+
+
+
+}
+
+
+
