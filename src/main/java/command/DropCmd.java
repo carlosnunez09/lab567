942611a370b790fd923be356dcb88270fe16994c
johnripper <jr2080@ship.edu>
2024-11-17 17:41:05 -0500

    adding others

--- src/main/java/command/DropCmd.java
+++ src/main/java/command/DropCmd.java
@@ -1,4 +1,27 @@
 package command;
 
+import environment.Cell;
+import environment.Environment;
+import lifeform.LifeForm;
+
 public class DropCmd {
+
+  private Environment environment;
+
+  public DropCmd(Environment environment){ this.environment = environment; }
+
+
+//  public void execute(int row, int col){
+//
+//    Cell cell = environment.getCell(row, col);
+//    LifeForm lifeForm = cell.getLifeForm();
+//
+//    if(lifeForm != null && lifeForm.hasWeapon()){
+//
+//
+//
+//    }
+//
+//  }
+
 }
