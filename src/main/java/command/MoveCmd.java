5003617b5510f211d61ea89042061fd4c6a355a3
Carlos Nunez <80909466+carlosnunez09@users.noreply.github.com>
2024-11-18 20:08:58 -0500

    Damage, removes damage but it stops after 4 shoots - im guessing is something to do with how shoots left work, each movement must bring it back/ round ends(i fixed it wile writeing this) - note we must end round/move before shooting more

--- src/main/java/command/MoveCmd.java
+++ src/main/java/command/MoveCmd.java
@@ -20,6 +20,7 @@ public class MoveCmd implements Command {
       LifeForm life = environment.getLifeForm(row, col);
       environment.getLifeForm(row, col).move(environment);
       environment.notifyObservers(life);
+      life.getWeapon().updateTime(1); // clue how it works,but it works, prob different implementation  for the future
       //System.out.println("from command: Moved" + " at row: " + row + " col: " + col); // testing to be removed
     }
   }
