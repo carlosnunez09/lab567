9dc1fbe4f7ad38953a94a82b88dc458e3aef2b47
johnripper <jr2080@ship.edu>
2024-11-25 16:29:59 -0500

    action state semi done

--- src/main/java/state/ActionState.java
+++ src/main/java/state/ActionState.java
@@ -1,4 +1,29 @@
 package state;
 
-public class ActionState {
+import environment.Environment;
+import lifeform.LifeForm;
+
+public abstract class ActionState extends {
+
+  protected AIContext context;
+  protected Environment e;
+  protected LifeForm lifeform;
+
+
+
+  public ActionState(AIContext context){
+    this.context = context;
+    this.e = context.getEnvironment();
+    this.lifeform = context.getLifeForm();
+
+  }
+
+  abstract void executeAction(){
+
+  }
+
+
+
+
+
 }
