f1c911aa293a357f2a5a3c770dc233695993ddd4
Carlos Nunez <cn4992@engr.ship.edu>
2024-11-30 23:16:50 -0500

    made some fixes to dead state, it still needs better tests that work

--- src/main/java/state/DeadState.java
+++ src/main/java/state/DeadState.java
@@ -1,6 +1,9 @@
 package state;
 
+import command.DropCmd;
 import exceptions.EnvironmentException;
+
+import java.util.Random;
 
 public class DeadState extends ActionState {
   AIContext ai;
@@ -13,14 +16,19 @@ public class DeadState extends ActionState {
   @Override
   public void executeAction() throws EnvironmentException {
     if (ai.getLifeForm().hasWeapon()) {
-      DropCommand drop = new DropCommand(ai.getEnvironment());
+      DropCmd drop = new DropCmd(ai.getEnvironment());
       drop.execute(ai.getLifeForm().getRow(), ai.getLifeForm().getCol());
     }
     ai.getEnvironment().removeLifeForm(ai.getLifeForm().getRow(), ai.getLifeForm().getCol());
     ai.getLifeForm().revive();
+    Random rand = new Random();
     boolean isAdded = true;
     while(!isAdded) {
-      isAdded = ai.getEnvironment().addLifeForm(ai.getLifeForm(),new RandInt(0,e.getNumRows() - 1).choose(),new RandInt(0,e.getNumCols()).choose() - 1);
+      isAdded = ai.getEnvironment().addLifeForm(
+              ai.getLifeForm(),
+              rand.nextInt(e.getNumRows() - 1),
+              rand.nextInt( e.getNumCols() - 1)
+      );
     }
     ai.setCurrentState(ai.getNoWeaponState());
     e.notifyObservers(ai.getLifeForm());
