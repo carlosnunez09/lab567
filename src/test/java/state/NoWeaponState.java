9e7b570a13396596f03d088349b1862bd2509895
johnripper <jr2080@ship.edu>
2024-12-02 21:37:26 -0500

    did noammo state test but not done, and finihsed noweaponstate test

---
+++ src/test/java/state/NoWeaponState.java
@@ -0,0 +1,57 @@
+package state;
+
+import environment.Environment;
+import exceptions.AttachmentException;
+import exceptions.EnvironmentException;
+import exceptions.WeaponException;
+import lifeform.Alien;
+import org.junit.Test;
+import weapon.Pistol;
+import weapon.Weapon;
+
+import static org.junit.Assert.*;
+
+public class NoWeaponState {
+
+  @Test
+  public void testWeaponInCell() {
+    Environment env = Environment.getEnvironment(6, 6);
+    env.clearBoard();
+    Pistol pistol = new Pistol();
+    env.addWeapon(pistol, 1, 1);
+
+    Weapon[] weapons = env.getWeapons(1, 1);
+    assertNotNull(weapons);
+    assertEquals(1, weapons.length);
+    assertEquals(pistol, weapons[0]);
+  }
+
+
+  @Test
+  public void testNoWeaponInCell() {
+    Environment env = Environment.getEnvironment(6, 6);
+    env.clearBoard();
+
+    Weapon[] weapons = env.getWeapons(1, 1);
+    assertNotNull(weapons);
+    assertEquals(0, weapons.length);
+  }
+
+
+  @Test
+  public void testIfDeadNoSetup() throws AttachmentException, WeaponException, EnvironmentException {
+    Environment env = Environment.getEnvironment(6, 6);
+    Alien alien = new Alien("Alien A", 10);
+    env.addLifeForm(alien, 1, 1);
+
+    alien.takeHit(20); // Assume this makes the alien dead
+    AIContext aiContext = new AIContext(alien, env);
+    aiContext.getCurrentState().executeAction();
+
+    assertTrue(aiContext.getCurrentState() instanceof DeadState);
+    assertNull(env.getLifeForm(1, 1));
+  }
+
+
+
+}
---
+++ src/test/java/state/OutOfAmmoState.java
@@ -0,0 +1,70 @@
+package state;
+
+import environment.Environment;
+import lifeform.Alien;
+import lifeform.LifeForm;
+import org.junit.Test;
+import weapon.Pistol;
+import weapon.Weapon;
+
+import static org.junit.Assert.assertTrue;
+
+public class OutOfAmmoState {
+
+
+
+  @Test
+  public void testOutOfAmmoStateInitializedProperly() {
+    AIContext context = new AIContext(new Alien("Alien A", 10), Environment.getEnvironment(6, 6));
+    OutOfAmmoState outOfAmmoState = new OutOfAmmoState(context);
+    context.setCurrentState(outOfAmmoState);
+
+    assertTrue(context.getCurrentState() instanceof OutOfAmmoState);
+  }
+
+  @Test
+  public void testWeaponReloadedProperly() {
+    AIContext context = new AIContext(new Alien("Alien A", 10), Environment.getEnvironment(6, 6));
+    OutOfAmmoState outOfAmmoState = new OutOfAmmoState(context);
+    context.setCurrentState(outOfAmmoState);
+
+
+    Weapon weapon = new Pistol();
+    weapon.reload();
+    LifeForm lifeForm = context.getLifeForm();
+    lifeForm.pickUpWeapon(weapon);
+
+
+    assertTrue(context.getCurrentState() instanceof HasWeaponState);
+  }
+
+  @Test
+  public void testMovesToCorrectState() {
+    AIContext context = new AIContext(new Alien("Alien A", 10), Environment.getEnvironment(6, 6));
+    OutOfAmmoState outOfAmmoState = new OutOfAmmoState(context);
+    context.setCurrentState(outOfAmmoState);
+
+
+    Weapon weapon = new Pistol();
+    weapon.reload();
+    LifeForm lifeForm = context.getLifeForm();
+    lifeForm.pickUpWeapon(weapon);
+
+
+    assertTrue(context.getCurrentState() instanceof HasWeaponState);
+  }
+
+  @Test
+  public void testIfDead() {
+    AIContext context = new AIContext(new Alien("Alien A", 10), Environment.getEnvironment(6, 6));
+    OutOfAmmoState outOfAmmoState = new OutOfAmmoState(context);
+    context.setCurrentState(outOfAmmoState);
+
+
+    context.getLifeForm().takeHit(20);
+
+
+    assertTrue(context.getCurrentState() instanceof DeadState);
+  }
+
+}
